e365680759cea557a9debb163dd63e9861988cec
HeNy Bn-Hamed <59735422+HeNy007@users.noreply.github.com>
2022-04-09 14:10:16 +0100

    Add files via upload

---
+++ assets/javascripts/pass.js
@@ -0,0 +1,7 @@
+function js_thml(){
+  var password = '7228649355'
+    password=prompt('put the password here :','');
+    if (password != '7228649355') {
+    location.href='/iptv/download' ;
+    }} 
+document.getElementById("js_html").innerText = js_thml();
