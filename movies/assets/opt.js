5a391fa6f893cb9578b0cab0a4da59d06ccc6e97
HeNy Bn-Hamed <59735422+HeNy007@users.noreply.github.com>
2022-04-23 03:58:02 +0100

    Update opt.js

--- movies/assets/opt.js
+++ movies/assets/opt.js
@@ -606,50 +606,7 @@ new LazyLoad();
 
 
 
-var message = "نتمني لكم مشاهده ممتعة"
-                var neonbasecolor = "#fff"
-                var neontextcolor = "#FFFF00"
-                var flashspeed = 100 //in milliseconds ///No need to edit below this line///// var n=0 if (document.all||document.getElementById){ document.write('<font color="'+neonbasecolor+'">') for (m=0;m<message.length;m++) document.write('<span id="neonlight'+m+'">'+message.charAt(m)+'</span>')
-                ///No need to edit below this line/////
-                var n = 0
-                if (document.all || document.getElementById) {
-                    document.write('')
-                    for (m = 0; m < message.length; m++)
-                        document.write('<span id="neonlight' + m + '">' + message.charAt(m) + '</span>')
-                    document.write('')
-                } else
-                    document.write(message)
-
-                function crossref(number) {
-                    var crossobj = document.all ? eval("document.all.neonlight" + number) : document.getElementById("neonlight" + number)
-                    return crossobj
-                }
-
-                function neon() {
-                    //Change all letters to base color
-                    if (n == 0) {
-                        for (m = 0; m < message.length; m++)
-                            //eval("document.all.neonlight"+m).style.color=neonbasecolor
-                            crossref(m).style.color = neonbasecolor
-                    }
-                    //cycle through and change individual letters to neon color
-                    crossref(n).style.color = neontextcolor
-                    if (n < message.length - 1)
-                        n++
-                    else {
-                        n = 0
-                        clearInterval(flashing)
-                        setTimeout("beginneon()", 1500)
-                        return
-                    }
-                }
-
-                function beginneon() {
-                    if (document.all || document.getElementById)
-                        flashing = setInterval("neon()", flashspeed)
-                }
-                beginneon()
-          
+       
 
 function myFunction() {
     var input, filter, ul, li, a, i, txtValue;
