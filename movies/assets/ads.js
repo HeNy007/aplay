4a537d7cf63fd5397303f2daf9170501220c6bf9
HeNy Bn-Hamed <59735422+HeNy007@users.noreply.github.com>
2022-04-30 09:04:07 +0100

    Update ads.js

--- movies/assets/ads.js
+++ movies/assets/ads.js
@@ -52,16 +52,3 @@ document.onmousedown = disableclick;
     }
   }
 
-//To Disable ctrl+c, ctrl+u
-
-jQuery(document).ready(function($){
-$(document).keydown(function(event) {
-var pressedKey = String.fromCharCode(event.keyCode).toLowerCase();
-
-if (event.ctrlKey && (pressedKey == "c" || pressedKey == "u")) {
-alert('Sorry, This Functionality Has Been Disabled!');
-//disable key press porcessing
-return false;
-}
-});
-});
