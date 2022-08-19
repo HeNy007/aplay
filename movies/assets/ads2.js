b2506c0c9b57b3c400ff6a37cb62dd08b5490bb8
HeNy Bn-Hamed <59735422+HeNy007@users.noreply.github.com>
2022-08-19 16:36:13 +0100

    Update ads2.js

--- movies/assets/ads2.js
+++ movies/assets/ads2.js
@@ -5,7 +5,7 @@ document.writeln("</style>");
 document.writeln("<div id=\"ad_asd\" class=\"slide_in\" style=\"display: none;\">");
 document.writeln("<span title=\"Close\" id=\"close_ad\">X</span>");
 document.writeln("<div class=\"center_ad\">");
-document.writeln("<iframe data-aa='1998808' src='//ad.a-ads.com/1998808?size=300x250' style='width:300px; height:250px; border:0px; padding:0; overflow:hidden; background-color: transparent;'></iframe>");
+document.writeln("<iframe data-aa='2065631' src='//ad.a-ads.com/2065631?size=300x250' style='width:300px; height:250px; border:0px; padding:0; overflow:hidden; background-color: transparent;'></iframe>");
 document.writeln("</div>");
 document.writeln("</div>");
 var elem=document.getElementById("close_ad");elem.addEventListener("click touchstart",handler,!1);elem.addEventListener("click",handler,!1);function handler(b){var a=document.getElementById("ad_asd");b=a.classList.contains("slide_in");setTimeout(function(){a.parentNode.removeChild(a)},500);a.setAttribute("class",b?"slide_out":"slide_in")};setTimeout(function(){document.getElementById("ad_asd").style.display="block"},3E3);
