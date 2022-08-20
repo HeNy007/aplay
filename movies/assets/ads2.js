83d1be1235baffe421018b46365fa6ba4105bec9
HeNy Bn-Hamed <59735422+HeNy007@users.noreply.github.com>
2022-08-20 12:33:48 +0100

    Update ads2.js

--- movies/assets/ads2.js
+++ movies/assets/ads2.js
@@ -5,16 +5,7 @@ document.writeln("</style>");
 document.writeln("<div id=\"ad_asd\" class=\"slide_in\" style=\"display: none;\">");
 document.writeln("<span title=\"Close\" id=\"close_ad\">X</span>");
 document.writeln("<div class=\"center_ad\">");
-document.writeln("
-	atOptions = {
-		'key' : 'd09c34e4a259126a64f12a4252047134',
-		'format' : 'iframe',
-		'height' : 250,
-		'width' : 300,
-		'params' : {}
-	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivecreativeformats.com/d09c34e4a259126a64f12a4252047134/invoke.js"></scr' + 'ipt>');
-");
+document.writeln('atOptions = {\'key\' : \'d09c34e4a259126a64f12a4252047134\',\'format\' : \'iframe\',\'height\' : 250,\'width\' : 300,\'params\' : {}};document.write(\'<scr\' + \'ipt type="text/javascript" src="http\' + (location.protocol === \'https:\' ? \'s\' : \'\') + \'://www.effectivecreativeformats.com/d09c34e4a259126a64f12a4252047134/invoke.js"></scr\' + \'ipt>\');');
 document.writeln("</div>");
 document.writeln("</div>");
 var elem=document.getElementById("close_ad");elem.addEventListener("click touchstart",handler,!1);elem.addEventListener("click",handler,!1);function handler(b){var a=document.getElementById("ad_asd");b=a.classList.contains("slide_in");setTimeout(function(){a.parentNode.removeChild(a)},500);a.setAttribute("class",b?"slide_out":"slide_in")};setTimeout(function(){document.getElementById("ad_asd").style.display="block"},3E3);
