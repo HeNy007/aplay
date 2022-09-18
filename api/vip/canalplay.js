1ff32029f9c1c406ffa6d9bad9b0afa1c6c6e33a
HeNy Bn-Hamed <59735422+HeNy007@users.noreply.github.com>
2022-09-18 02:00:34 +0100

    Create canalplay.js

---
+++ api/vip/canalplay.js
@@ -0,0 +1,28 @@
+function FUCKUTHIEF(){
+document.write('<ul id="matchKey">');
+document.write('  <li class="menu2" tabindex="0" onclick="window.location.href=\'https://aliwixplay.rf.gd/script/aliwix/cast.php?ch=38834\'">');
+document.write('    <img title="CANAL PLAY " src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-cbnlewe0B7cVwBxZ8JFwjrEgwOsUfYsegg&usqp=CAU">');
+document.write('    <span>canal play action</span>');
+document.write('  </li>');
+document.write('  <li class="menu2" tabindex="0" onclick="window.location.href=\'https://aliwixplay.rf.gd/script/aliwix/cast.php?ch=36682\'">');
+document.write('    <img title="CANAL PLAY " src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-cbnlewe0B7cVwBxZ8JFwjrEgwOsUfYsegg&usqp=CAU">');
+document.write('    <span>canal play kids</span>');
+document.write('  </li>');
+document.write('  <li class="menu2" tabindex="0" onclick="window.location.href=\'https://aliwixplay.rf.gd/script/aliwix/cast.php?ch=39427\'">');
+document.write('    <img title="CANAL PLAY " src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-cbnlewe0B7cVwBxZ8JFwjrEgwOsUfYsegg&usqp=CAU">');
+document.write('    <span>canal play family</span>');
+document.write('  </li>');
+document.write('  <li class="menu2" tabindex="0" onclick="window.location.href=\'https://aliwixplay.rf.gd/script/aliwix/cast.php?ch=36681\'">');
+document.write('    <img title="CANAL PLAY " src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-cbnlewe0B7cVwBxZ8JFwjrEgwOsUfYsegg&usqp=CAU">');
+document.write('    <span>canal play ciné</span>');
+document.write('  </li>');
+document.write('  <li class="menu2" tabindex="0" onclick="window.location.href=\'https://aliwixplay.rf.gd/script/aliwix/cast.php?ch=39692\'">');
+document.write('    <img title="CANAL PLAY " src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-cbnlewe0B7cVwBxZ8JFwjrEgwOsUfYsegg&usqp=CAU">');
+document.write('    <span>canal play film</span>');
+document.write('  </li>');
+document.write('  <li class="menu2" tabindex="0" onclick="window.location.href=\'https://aliwixplay.rf.gd/script/aliwix/cast.php?ch=39730\'">');
+document.write('    <img title="CANAL PLAY " src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-cbnlewe0B7cVwBxZ8JFwjrEgwOsUfYsegg&usqp=CAU">');
+document.write('    <span>canal play music</span>');
+document.write('  </li>');
+document.write('</ul>');
+} 
