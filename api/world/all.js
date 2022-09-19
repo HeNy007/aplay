f78a713c39bd1f8bb267fb33f8a3500d0784b905
HeNy Bn-Hamed <59735422+HeNy007@users.noreply.github.com>
2022-09-19 16:29:22 +0100

    Create all.js

---
+++ api/world/all.js
@@ -0,0 +1,8 @@
+function FUCKUTHIEF(){
+document.write('<ul id="matchKey">');
+document.write('  <li class="menu" tabindex="0" onclick="window.location.href=\'go:france\'">');
+document.write('    <img title="FRANCE " src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-cbnlewe0B7cVwBxZ8JFwjrEgwOsUfYsegg&usqp=CAU">');
+document.write('    <span>tf1</span>');
+document.write('  </li>');
+
+} 
