8ceddd5279bfc13a558237d5251d25225251a0c6
HeNy Bn-Hamed <59735422+HeNy007@users.noreply.github.com>
2022-11-19 20:17:41 +0100

    Create word.js

---
+++ api/sports/bein/word.js
@@ -0,0 +1,82 @@
+function FUCKUTHIEF(){
+document.write('<ul id="matchKey">');
+
+document.write('  <li class="menu2" tabindex="0" onclick="window.location.href=\'http://aliwixplay.rf.gd/script/live6.php?ch=1\'">');
+document.write('    <img title="CHOALA SP" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHZWlT1Ogba1vE2NoTlL-oJE2p9LDteSbQLw&usqp=CAU">');
+document.write('    <span>CHOALA SP 1</span>');
+document.write('  </li>');
+
+document.write('  <li class="menu2" tabindex="0" onclick="window.location.href=\'http://aliwixplay.rf.gd/script/live6.php?ch=2\'">');
+document.write('    <img title="CHOALA SP" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHZWlT1Ogba1vE2NoTlL-oJE2p9LDteSbQLw&usqp=CAU">');
+document.write('    <span>CHOALA SP 2</span>');
+document.write('  </li>');
+
+document.write('  <li class="menu2" tabindex="0" onclick="window.location.href=\'http://aliwixplay.rf.gd/script/live6.php?ch=3\'">');
+document.write('    <img title="CHOALA SP" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHZWlT1Ogba1vE2NoTlL-oJE2p9LDteSbQLw&usqp=CAU">');
+document.write('    <span>CHOALA SP 3</span>');
+document.write('  </li>');
+
+document.write('  <li class="menu2" tabindex="0" onclick="window.location.href=\'http://aliwixplay.rf.gd/script/live6.php?ch=4\'">');
+document.write('    <img title="CHOALA SP" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHZWlT1Ogba1vE2NoTlL-oJE2p9LDteSbQLw&usqp=CAU">');
+document.write('    <span>CHOALA SP 4</span>');
+document.write('  </li>');
+
+document.write('  <li class="menu2" tabindex="0" onclick="window.location.href=\'https://aliwixplay.tn/player/qaliwix?src=1\'">');
+document.write('    <img title="BEIN SP" src="https://beinsportslivehd.com/wp-content/uploads/2022/03/cropped-logo-2.jpg">');
+document.write('    <span>beIN Live 1</span>');
+document.write('  </li>');
+
+document.write('  <li class="menu2" tabindex="0" onclick="window.location.href=\'https://aliwixplay.tn/player/qaliwix?src=2\'">');
+document.write('    <img title="BEIN SP" src="https://beinsportslivehd.com/wp-content/uploads/2022/03/cropped-logo-2.jpg">');
+document.write('    <span>beIN Live 2</span>');
+document.write('  </li>');
+
+document.write('  <li class="menu2" tabindex="0" onclick="window.location.href=\'https://aliwixplay.tn/player/qaliwix?src=3\'">');
+document.write('    <img title="BEIN SP" src="https://beinsportslivehd.com/wp-content/uploads/2022/03/cropped-logo-2.jpg">');
+document.write('    <span>beIN Live 3</span>');
+document.write('  </li>');
+
+document.write('  <li class="menu2" tabindex="0" onclick="window.location.href=\'https://aliwixplay.tn/player/qaliwix?src=4\'">');
+document.write('    <img title="BEIN SP" src="https://beinsportslivehd.com/wp-content/uploads/2022/03/cropped-logo-2.jpg">');
+document.write('    <span>beIN Live 4</span>');
+document.write('  </li>');
+
+document.write('  <li class="menu2" tabindex="0" onclick="window.location.href=\'https://aliwixplay.tn/player/qaliwix?src=5\'">');
+document.write('    <img title="BEIN SP" src="https://beinsportslivehd.com/wp-content/uploads/2022/03/cropped-logo-2.jpg">');
+document.write('    <span>beIN Live 5</span>');
+document.write('  </li>');
+
+document.write('  <li class="menu2" tabindex="0" onclick="window.location.href=\'https://aliwixplay.tn/player/qaliwix?src=6\'">');
+document.write('    <img title="BEIN SP" src="https://beinsportslivehd.com/wp-content/uploads/2022/03/cropped-logo-2.jpg">');
+document.write('    <span>beIN Live 6</span>');
+document.write('  </li>');
+
+document.write('  <li class="menu2" tabindex="0" onclick="window.location.href=\'http://aliwixplay.rf.gd/script/live6.php?ch=63\'">');
+document.write('    <img title="CHOALA SP" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrbcBWjL5RsuSNk0LCIGC9r2r-FHh0rRsMAQ&usqp=CAU">');
+document.write('    <span>salam tv PS</span>');
+document.write('  </li>');
+
+document.write('  <li class="menu2" tabindex="0" onclick="window.location.href=\'http://aliwixplay.rf.gd/script/live6.php?ch=59\'">');
+document.write('    <img title="CHOALA SP" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMvf-d_KmgGrVPogyvQ-QD5RMxPLgeMFHH6w&usqp=CAU>');
+document.write('    <span>Alhorya sport PS</span>');
+document.write('  </li>');
+
+document.write('  <li class="menu2" tabindex="0" onclick="window.location.href=\'http://aliwixplay.rf.gd/script/live6.php?ch=44\'">');
+document.write('    <img title="CHOALA SP" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQseJqAWQgvC01h5zggs7wfjQfQ7l4kJqQB8A&usqp=CAU">');
+document.write('    <span>Raya 1 PS</span>');
+document.write('  </li>');
+
+document.write('  <li class="menu2" tabindex="0" onclick="window.location.href=\'http://aliwixplay.rf.gd/script/live6.php?ch=45\'">');
+document.write('    <img title="CHOALA SP" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQseJqAWQgvC01h5zggs7wfjQfQ7l4kJqQB8A&usqp=CAU">');
+document.write('    <span>Raya 2 PS</span>');
+document.write('  </li>');
+
+document.write('  <li class="menu2" tabindex="0" onclick="window.location.href=\'http://aliwixplay.rf.gd/script/live6.php?ch=46\'">');
+document.write('    <img title="CHOALA SP" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQseJqAWQgvC01h5zggs7wfjQfQ7l4kJqQB8A&usqp=CAU">');
+document.write('    <span>Raya 3 PS</span>');
+document.write('  </li>');
+
+
+
+document.write('</ul>');
+} 
