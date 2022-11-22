6737e5cb1d79d784afa7e857645a6bf1ae10a34b
HeNy Bn-Hamed <59735422+HeNy007@users.noreply.github.com>
2022-11-22 17:19:16 +0100

    Update word.js

--- api/sports/bein/word.js
+++ api/sports/bein/word.js
@@ -1,44 +1,6 @@
 function FUCKUTHIEF(){
 document.write('<ul id="matchKey">');
 
-
-
-document.write('  <li class="menu2" tabindex="0" onclick="window.location.href=\'https://aliwixplay.tn/player/qaliwix?src=1\'">');
-document.write('    <img title="BEIN SP" src="https://beinsportslivehd.com/wp-content/uploads/2022/03/cropped-logo-2.jpg">');
-document.write('    <span>beIN Live 1</span>');
-document.write('  </li>');
-
-document.write('  <li class="menu2" tabindex="0" onclick="window.location.href=\'https://aliwixplay.tn/player/qaliwix?src=2\'">');
-document.write('    <img title="BEIN SP" src="https://beinsportslivehd.com/wp-content/uploads/2022/03/cropped-logo-2.jpg">');
-document.write('    <span>beIN Live 2</span>');
-document.write('  </li>');
-
-document.write('  <li class="menu2" tabindex="0" onclick="window.location.href=\'https://aliwixplay.tn/player/qaliwix?src=3\'">');
-document.write('    <img title="BEIN SP" src="https://beinsportslivehd.com/wp-content/uploads/2022/03/cropped-logo-2.jpg">');
-document.write('    <span>beIN Live 3</span>');
-document.write('  </li>');
-
-document.write('  <li class="menu2" tabindex="0" onclick="window.location.href=\'https://aliwixplay.tn/player/qaliwix?src=4\'">');
-document.write('    <img title="BEIN SP" src="https://beinsportslivehd.com/wp-content/uploads/2022/03/cropped-logo-2.jpg">');
-document.write('    <span>beIN Live 4</span>');
-document.write('  </li>');
-
-document.write('  <li class="menu2" tabindex="0" onclick="window.location.href=\'https://aliwixplay.tn/player/qaliwix?src=5\'">');
-document.write('    <img title="BEIN SP" src="https://beinsportslivehd.com/wp-content/uploads/2022/03/cropped-logo-2.jpg">');
-document.write('    <span>beIN Live 5</span>');
-document.write('  </li>');
-
-document.write('  <li class="menu2" tabindex="0" onclick="window.location.href=\'https://aliwixplay.tn/player/qaliwix?src=6\'">');
-document.write('    <img title="BEIN SP" src="https://beinsportslivehd.com/wp-content/uploads/2022/03/cropped-logo-2.jpg">');
-document.write('    <span>beIN Live 6</span>');
-document.write('  </li>');
-
-document.write('  <li class="menu2" tabindex="0" onclick="window.location.href=\'https://aliwixplay.tn/player/qaliwix?src=7\'">');
-document.write('    <img title="BEIN SP" src="https://beinsportslivehd.com/wp-content/uploads/2022/03/cropped-logo-2.jpg">');
-document.write('    <span>beIN Live 7</span>');
-document.write('  </li>');
-
-
 document.write('  <li class="menu2" tabindex="0" onclick="window.location.href=\'http://aliwixplay.rf.gd/script/live6.php?ch=1\'">');
 document.write('    <img title="CHOALA SP" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHZWlT1Ogba1vE2NoTlL-oJE2p9LDteSbQLw&usqp=CAU">');
 document.write('    <span>CHOALA SP 1</span>');
@@ -84,6 +46,45 @@ document.write('  <li class="menu2" tabindex="0" onclick="window.location.href=\
 document.write('    <img title="CHOALA SP" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQseJqAWQgvC01h5zggs7wfjQfQ7l4kJqQB8A&usqp=CAU">');
 document.write('    <span>Raya 3 PS</span>');
 document.write('  </li>');
+
+
+
+document.write('  <li class="menu2" tabindex="0" onclick="window.location.href=\'https://aliwixplay.tn/player/qaliwix?src=1\'">');
+document.write('    <img title="BEIN SP" src="https://beinsportslivehd.com/wp-content/uploads/2022/03/cropped-logo-2.jpg">');
+document.write('    <span>beIN Live 1</span>');
+document.write('  </li>');
+
+document.write('  <li class="menu2" tabindex="0" onclick="window.location.href=\'https://aliwixplay.tn/player/qaliwix?src=2\'">');
+document.write('    <img title="BEIN SP" src="https://beinsportslivehd.com/wp-content/uploads/2022/03/cropped-logo-2.jpg">');
+document.write('    <span>beIN Live 2</span>');
+document.write('  </li>');
+
+document.write('  <li class="menu2" tabindex="0" onclick="window.location.href=\'https://aliwixplay.tn/player/qaliwix?src=3\'">');
+document.write('    <img title="BEIN SP" src="https://beinsportslivehd.com/wp-content/uploads/2022/03/cropped-logo-2.jpg">');
+document.write('    <span>beIN Live 3</span>');
+document.write('  </li>');
+
+document.write('  <li class="menu2" tabindex="0" onclick="window.location.href=\'https://aliwixplay.tn/player/qaliwix?src=4\'">');
+document.write('    <img title="BEIN SP" src="https://beinsportslivehd.com/wp-content/uploads/2022/03/cropped-logo-2.jpg">');
+document.write('    <span>beIN Live 4</span>');
+document.write('  </li>');
+
+document.write('  <li class="menu2" tabindex="0" onclick="window.location.href=\'https://aliwixplay.tn/player/qaliwix?src=5\'">');
+document.write('    <img title="BEIN SP" src="https://beinsportslivehd.com/wp-content/uploads/2022/03/cropped-logo-2.jpg">');
+document.write('    <span>beIN Live 5</span>');
+document.write('  </li>');
+
+document.write('  <li class="menu2" tabindex="0" onclick="window.location.href=\'https://aliwixplay.tn/player/qaliwix?src=6\'">');
+document.write('    <img title="BEIN SP" src="https://beinsportslivehd.com/wp-content/uploads/2022/03/cropped-logo-2.jpg">');
+document.write('    <span>beIN Live 6</span>');
+document.write('  </li>');
+
+document.write('  <li class="menu2" tabindex="0" onclick="window.location.href=\'https://aliwixplay.tn/player/qaliwix?src=7\'">');
+document.write('    <img title="BEIN SP" src="https://beinsportslivehd.com/wp-content/uploads/2022/03/cropped-logo-2.jpg">');
+document.write('    <span>beIN Live 7</span>');
+document.write('  </li>');
+
+
 
 
 
