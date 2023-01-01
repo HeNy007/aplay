6315c24b9772ab3b78995b490b5227d2263a3506
HeNy Bn-Hamed <59735422+HeNy007@users.noreply.github.com>
2023-01-01 14:32:16 +0100

    Create danger.js

---
+++ player/danger.js
@@ -0,0 +1,14 @@
+
+
+function gup(e){e=e.replace(/[\[]/,"\\[").replace(/[\]]/,"\\]");
+var n="[\\?&]"+e+"=([^&#]*)",
+l=new RegExp(n),
+c=l.exec(window.location.href);
+return null==c?"":unescape(c[1])}
+var year=gup("year");
+var dis=gup("disc");
+var disc=atob(dis);
+var tit=gup("title");
+var title=atob(tit);
+var vid=gup("src");
+var cover=gup("cover");
