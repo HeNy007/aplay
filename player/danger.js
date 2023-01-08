8828ec80f201880afd3fcd36e61f387b4584bdc8
HeNy Bn-Hamed <59735422+HeNy007@users.noreply.github.com>
2023-01-08 16:14:20 +0100

    Update danger.js

--- player/danger.js
+++ player/danger.js
@@ -1,5 +1,4 @@
-
-
+document.write('<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.2/css/all.css"/><a href="https://t.me/AliwixPlay" class="tel" target="_blank"><i class="fab fa-telegram t"></i></a><style>.tel{position:fixed;width:60px;height:60px;bottom:40px;left:50px;background-color:#57b7eb;color:#FFF;border-radius:50px;text-align:center;font-size:40px;z-index:100} .t{color:#FFF;margin-top:10px} .tel{animation:pulse 2s infinite} .tel:hover{box-shadow: 2px 2px 11px rgb(0 0 0 / 70%);}   @keyframes btnun-what{10%{transform:translate(0,200px)}50%{transform:translate(0,-40px)}70%{transform:scale(1.1)}} @keyframes pulse{50%{transform:scale(1.1)}}   </style>');
 function gup(e){e=e.replace(/[\[]/,"\\[").replace(/[\]]/,"\\]");
 var n="[\\?&]"+e+"=([^&#]*)",
 l=new RegExp(n),
