6158f0c6968c8ab8ba8f127d70719a2982db30c5
HeNy Bn-Hamed <59735422+HeNy007@users.noreply.github.com>
2023-01-08 01:01:03 +0100

    Update danger.js

--- player/danger.js
+++ player/danger.js
@@ -5,10 +5,11 @@ var n="[\\?&]"+e+"=([^&#]*)",
 l=new RegExp(n),
 c=l.exec(window.location.href);
 return null==c?"":unescape(c[1])}
-var year=gup("year");
+var time=gup("time");
 var dis=gup("disc");
 var disc=atob(dis);
 var tit=gup("title");
 var title=atob(tit);
 var vid=gup("src");
 var cover=gup("cover");
+var trailer=gup("trailer");
