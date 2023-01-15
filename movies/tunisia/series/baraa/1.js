e8d02abec3fe494e4c9ab3bf8b6f47b4f61f2e96
HeNy Bn-Hamed <59735422+HeNy007@users.noreply.github.com>
2023-01-15 10:19:12 +0100

    Update 1.js

--- movies/tunisia/series/baraa/1.js
+++ movies/tunisia/series/baraa/1.js
@@ -1,19 +1,15 @@
 document.getElementsByTagName('meta')["keywords"].content = "My new page keywords!!";
 document.getElementsByTagName('meta')["description"].content = "My new page description!!";
 document.title = "My new Document Title!!";
-$('meta[property=og\\:image]').attr('content', 'http://myweb.com/image.jpg');
-$('meta[property=og\\:title]').attr('content', 'title here');. $('meta[property=og\\:description]').attr('content', 'description here');
-
-
 
 function FUCKU(){
 document.write('<i class="bi bi-list-nested"></i> قصه المسلسل <i class="bi bi-list-nested"></i>');
-document.write('<br> يبحث المسلسل في عدة نقاط جديدة طرأت على المجتمع التونسي بعد الثورة وظهور جماعة الإخوان، وتعتبر من الخطوط الحمراء كالزواج العرفي الذي يسعى إليه وناس، واعتبار تعدد الزوجات أمر شرعي لا غبار عليه. </div>undefined</div>undefined</a>undefined<a class="btn" href="http://aliwix-play.tn:8080/script/fck1.php?id=https://vidbam.org/embed-gva153byk5ai.html">undefined<img class="thumb" src="https://r.yaqot.me/upload/articles/5fcea399.png">');
+document.write('<br> يبحث المسلسل في عدة نقاط جديدة طرأت على المجتمع التونسي بعد الثورة وظهور جماعة الإخوان، وتعتبر من الخطوط الحمراء كالزواج العرفي الذي يسعى إليه وناس، واعتبار تعدد الزوجات أمر شرعي لا غبار عليه. </div></div></a><a class="btn" href="http://aliwix-play.tn:8080/script/fck1.php?id=https://vidbam.org/embed-gva153byk5ai.html"><img class="thumb" src="https://r.yaqot.me/upload/articles/5fcea399.png">');
 document.write('  <div class="video-8">');
 document.write('    <i class="bi bi-play-btn-fill"></i>');
 document.write('    <div class="episode"> الحلقة 1 </div>');
-document.write('  </div>undefined');
-document.write('</a>undefined<a class="btn" href="http://aliwix-play.tn:8080/script/fck1.php?id=https://vidbam.org/embed-obq46wnmbktv.html?i=1">');
+document.write('  </div>');
+document.write('</a><a class="btn" href="http://aliwix-play.tn:8080/script/fck1.php?id=https://vidbam.org/embed-obq46wnmbktv.html?i=1">');
 document.write('  <img class="thumb" src="https://r.yaqot.me/upload/articles/5fcea399.png">');
 document.write('  <div class="video-8">');
 document.write('    <i class="bi bi-play-btn-fill"></i>');
@@ -156,4 +152,3 @@ document.write('</a>');
 document.write('</div>');
 
 }
-
