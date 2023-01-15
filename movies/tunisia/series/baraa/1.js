97b3b52cedbefea00d82068d458823416144bc00
HeNy Bn-Hamed <59735422+HeNy007@users.noreply.github.com>
2023-01-15 10:10:29 +0100

    Update 1.js

--- movies/tunisia/series/baraa/1.js
+++ movies/tunisia/series/baraa/1.js
@@ -8,39 +8,152 @@ $('meta[property=og\\:title]').attr('content', 'title here');. $('meta[property=
 
 function FUCKU(){
 document.write('<i class="bi bi-list-nested"></i> قصه المسلسل <i class="bi bi-list-nested"></i>');
-document.write('<br> 
-
-يبحث المسلسل في عدة نقاط جديدة طرأت على المجتمع التونسي بعد الثورة وظهور جماعة الإخوان، وتعتبر من الخطوط الحمراء كالزواج العرفي الذي يسعى إليه وناس، واعتبار تعدد الزوجات أمر شرعي لا غبار عليه. 
-
-</div></div></a><a class="btn" href="http://aliwix-play.tn:8080/script/fck1.php?id=https://vidbam.org/embed-gva153byk5ai.html"><img class="thumb" src="https://r.yaqot.me/upload/articles/5fcea399.png">');
+document.write('<br> يبحث المسلسل في عدة نقاط جديدة طرأت على المجتمع التونسي بعد الثورة وظهور جماعة الإخوان، وتعتبر من الخطوط الحمراء كالزواج العرفي الذي يسعى إليه وناس، واعتبار تعدد الزوجات أمر شرعي لا غبار عليه. </div>undefined</div>undefined</a>undefined<a class="btn" href="http://aliwix-play.tn:8080/script/fck1.php?id=https://vidbam.org/embed-gva153byk5ai.html">undefined<img class="thumb" src="https://r.yaqot.me/upload/articles/5fcea399.png">');
 document.write('  <div class="video-8">');
 document.write('    <i class="bi bi-play-btn-fill"></i>');
-document.write('    <div class="episode">
-  الحلقة 1
-</div>');
-document.write('  </div>');
-document.write('</a>');
-document.write('<a class="btn" href="http://aliwix-play.tn:8080/script/fck1.php?id=https://vidbam.org/embed-obq46wnmbktv.html?i=1">');
+document.write('    <div class="episode"> الحلقة 1 </div>');
+document.write('  </div>undefined');
+document.write('</a>undefined<a class="btn" href="http://aliwix-play.tn:8080/script/fck1.php?id=https://vidbam.org/embed-obq46wnmbktv.html?i=1">');
 document.write('  <img class="thumb" src="https://r.yaqot.me/upload/articles/5fcea399.png">');
 document.write('  <div class="video-8">');
 document.write('    <i class="bi bi-play-btn-fill"></i>');
-document.write('    <div class="episode">
-  الحلقة 2
-</div>');
+document.write('    <div class="episode"> الحلقة 2 </div>');
 document.write('  </div>');
 document.write('</a>');
-
-
-
-
-
-
-
-
-
-
-
-
+document.write('<a class="btn" href="https://aplays.herokuapp.com/script/fck2.php?id=https://vidspeeds.com:2096/embed-8n4xg9z86ilv.html?i=1">');
+document.write('  <img class="thumb" src="https://r.yaqot.me/upload/articles/5fcea399.png">');
+document.write('  <div class="video-8">');
+document.write('    <i class="bi bi-play-btn-fill"></i>');
+document.write('    <div class="episode"> الحلقة 3 </div>');
+document.write('  </div>');
+document.write('</a>');
+document.write('<a class="btn" href="http://aliwix-play.tn:8080/script/fck1.php?id=https://vidbam.org/embed-n5u4wevyk2c0.html?i=1">');
+document.write('  <img class="thumb" src="https://r.yaqot.me/upload/articles/5fcea399.png">');
+document.write('  <div class="video-8">');
+document.write('    <i class="bi bi-play-btn-fill"></i>');
+document.write('    <div class="episode"> الحلقة 4 </div>');
+document.write('  </div>');
+document.write('</a>');
+document.write('<a class="btn" href="http://aliwix-play.tn:8080/script/fck1.php?id=https://vidbam.org/embed-lbjof3v5bz70.html?i=1">');
+document.write('  <img class="thumb" src="https://r.yaqot.me/upload/articles/5fcea399.png">');
+document.write('  <div class="video-8">');
+document.write('    <i class="bi bi-play-btn-fill"></i>');
+document.write('    <div class="episode"> الحلقة 5 </div>');
+document.write('  </div>');
+document.write('</a>');
+document.write('<a class="btn" href="https://aplays.herokuapp.com/script/fck2.php?id=https://vidspeeds.com:2096/embed-9xj8xk6j760r.html?i=1">');
+document.write('  <img class="thumb" src="https://r.yaqot.me/upload/articles/5fcea399.png">');
+document.write('  <div class="video-8">');
+document.write('    <i class="bi bi-play-btn-fill"></i>');
+document.write('    <div class="episode"> الحلقة 6 </div>');
+document.write('  </div>');
+document.write('</a>');
+document.write('<a class="btn" href="http://aliwix-play.tn:8080/script/fck1.php?id=https://vidbam.org/embed-vw3as7s5yt89.html?i=1">');
+document.write('  <img class="thumb" src="https://r.yaqot.me/upload/articles/5fcea399.png">');
+document.write('  <div class="video-8">');
+document.write('    <i class="bi bi-play-btn-fill"></i>');
+document.write('    <div class="episode"> الحلقة 7 </div>');
+document.write('  </div>');
+document.write('</a>');
+document.write('<a class="btn" href="http://aliwix-play.tn:8080/script/fck1.php?id=https://vidbam.org/embed-xb9jr3dg37pv.html?i=1">');
+document.write('  <img class="thumb" src="https://r.yaqot.me/upload/articles/5fcea399.png">');
+document.write('  <div class="video-8">');
+document.write('    <i class="bi bi-play-btn-fill"></i>');
+document.write('    <div class="episode"> الحلقة 8 </div>');
+document.write('  </div>');
+document.write('</a>');
+document.write('<a class="btn" href="http://aliwix-play.tn:8080/script/fck1.php?id=https://vidbam.org/embed-ham2jvtsw4tu.html?i=1">');
+document.write('  <img class="thumb" src="https://r.yaqot.me/upload/articles/5fcea399.png">');
+document.write('  <div class="video-8">');
+document.write('    <i class="bi bi-play-btn-fill"></i>');
+document.write('    <div class="episode"> الحلقة 9 </div>');
+document.write('  </div>');
+document.write('</a>');
+document.write('<a class="btn" href="http://aliwix-play.tn:8080/script/fck1.php?id=https://vidbam.org/embed-ns00l4tusqxs.html?i=1">');
+document.write('  <img class="thumb" src="https://r.yaqot.me/upload/articles/5fcea399.png">');
+document.write('  <div class="video-8">');
+document.write('    <i class="bi bi-play-btn-fill"></i>');
+document.write('    <div class="episode"> الحلقة 10 </div>');
+document.write('  </div>');
+document.write('</a>');
+document.write('<a class="btn" href="http://aliwix-play.tn:8080/script/fck1.php?id=https://vidbam.org/embed-j6s5qfsf9dgd.html?i=1">');
+document.write('  <img class="thumb" src="https://r.yaqot.me/upload/articles/5fcea399.png">');
+document.write('  <div class="video-8">');
+document.write('    <i class="bi bi-play-btn-fill"></i>');
+document.write('    <div class="episode"> الحلقة 11 </div>');
+document.write('  </div>');
+document.write('</a>');
+document.write('<a class="btn" href="http://aliwix-play.tn:8080/script/fck1.php?id=https://vidbam.org/embed-t2zgczgwh8l6.html?i=1">');
+document.write('  <img class="thumb" src="https://r.yaqot.me/upload/articles/5fcea399.png">');
+document.write('  <div class="video-8">');
+document.write('    <i class="bi bi-play-btn-fill"></i>');
+document.write('    <div class="episode"> الحلقة 12 </div>');
+document.write('  </div>');
+document.write('</a>');
+document.write('<a class="btn" href="http://aliwix-play.tn:8080/script/fck1.php?id=https://vidbam.org/embed-sz3mp0f7k0zs.html?i=1">');
+document.write('  <img class="thumb" src="https://r.yaqot.me/upload/articles/5fcea399.png">');
+document.write('  <div class="video-8">');
+document.write('    <i class="bi bi-play-btn-fill"></i>');
+document.write('    <div class="episode"> الحلقة 13 </div>');
+document.write('  </div>');
+document.write('</a>');
+document.write('<a class="btn" href="http://aliwix-play.tn:8080/script/fck1.php?id=https://vidbam.org/embed-kemcih6yn7sj.html?i=1">');
+document.write('  <img class="thumb" src="https://r.yaqot.me/upload/articles/5fcea399.png">');
+document.write('  <div class="video-8">');
+document.write('    <i class="bi bi-play-btn-fill"></i>');
+document.write('    <div class="episode"> الحلقة 14 </div>');
+document.write('  </div>');
+document.write('</a>');
+document.write('<a class="btn" href="http://aliwix-play.tn:8080/script/fck1.php?id=https://vidbam.org/embed-9ccdwjtlqowr.html?i=1">');
+document.write('  <img class="thumb" src="https://r.yaqot.me/upload/articles/5fcea399.png">');
+document.write('  <div class="video-8">');
+document.write('    <i class="bi bi-play-btn-fill"></i>');
+document.write('    <div class="episode"> الحلقة 15 </div>');
+document.write('  </div>');
+document.write('</a>');
+document.write('<a class="btn" href="http://aliwix-play.tn:8080/script/fck1.php?id=https://vidbam.org/embed-fb0hopxiq7kl.html?i=1">');
+document.write('  <img class="thumb" src="https://r.yaqot.me/upload/articles/5fcea399.png">');
+document.write('  <div class="video-8">');
+document.write('    <i class="bi bi-play-btn-fill"></i>');
+document.write('    <div class="episode"> الحلقة 16 </div>');
+document.write('  </div>');
+document.write('</a>');
+document.write('<a class="btn" href="http://aliwix-play.tn:8080/script/fck1.php?id=https://vidbam.org/embed-2ao0qm5e6je5.html?i=1">');
+document.write('  <img class="thumb" src="https://r.yaqot.me/upload/articles/5fcea399.png">');
+document.write('  <div class="video-8">');
+document.write('    <i class="bi bi-play-btn-fill"></i>');
+document.write('    <div class="episode"> الحلقة 17 </div>');
+document.write('  </div>');
+document.write('</a>');
+document.write('<a class="btn" href="http://aliwix-play.tn:8080/script/fck1.php?id=https://vidbam.org/embed-gx2soc1ommj7.html?i=1">');
+document.write('  <img class="thumb" src="https://r.yaqot.me/upload/articles/5fcea399.png">');
+document.write('  <div class="video-8">');
+document.write('    <i class="bi bi-play-btn-fill"></i>');
+document.write('    <div class="episode"> الحلقة 18 </div>');
+document.write('  </div>');
+document.write('</a>');
+document.write('<a class="btn" href="http://aliwix-play.tn:8080/script/fck1.php?id=https://vidbam.org/embed-jojzvr4dxcnk.html?i=1">');
+document.write('  <img class="thumb" src="https://r.yaqot.me/upload/articles/5fcea399.png">');
+document.write('  <div class="video-8">');
+document.write('    <i class="bi bi-play-btn-fill"></i>');
+document.write('    <div class="episode"> الحلقة 19 </div>');
+document.write('  </div>');
+document.write('</a>');
+document.write('<a class="btn" href="http://aliwix-play.tn:8080/script/fck1.php?id=https://vidbam.org/embed-mv9hl7t0jqma.html?i=1">');
+document.write('  <img class="thumb" src="https://r.yaqot.me/upload/articles/5fcea399.png">');
+document.write('  <div class="video-8">');
+document.write('    <i class="bi bi-play-btn-fill"></i>');
+document.write('    <div class="episode"> الحلقة 20 </div>');
+document.write('  </div>');
+document.write('</a>');
+document.write('<a class="btn" href="http://aliwix-play.tn:8080/script/fck1.php?id=https://vidbam.org/embed-olyrbse31v6z.html?i=1">');
+document.write('  <img class="thumb" src="https://r.yaqot.me/upload/articles/5fcea399.png">');
+document.write('  <div class="video-8">');
+document.write('    <i class="bi bi-play-btn-fill"></i>');
+document.write('    <div class="episode"> الحلقة 21 </div>');
+document.write('  </div>');
+document.write('</a>');
 document.write('</div>');
+
 }
 
