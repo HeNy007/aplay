61d151d3d8ac4d8cb0ca05cd4d0206d40e8f3c69
HeNy Bn-Hamed <59735422+HeNy007@users.noreply.github.com>
2023-01-15 09:38:32 +0100

    Update and rename 1.html to 1.js

---
+++ movies/tunisia/series/baraa/1.js
@@ -0,0 +1,46 @@
+document.getElementsByTagName('meta')["keywords"].content = "My new page keywords!!";
+document.getElementsByTagName('meta')["description"].content = "My new page description!!";
+document.title = "My new Document Title!!";
+$('meta[property=og\\:image]').attr('content', 'http://myweb.com/image.jpg');
+$('meta[property=og\\:title]').attr('content', 'title here');. $('meta[property=og\\:description]').attr('content', 'description here');
+
+
+
+function FUCKU(){
+document.write('<i class="bi bi-list-nested"></i> قصه المسلسل <i class="bi bi-list-nested"></i>');
+document.write('<br> 
+
+يبحث المسلسل في عدة نقاط جديدة طرأت على المجتمع التونسي بعد الثورة وظهور جماعة الإخوان، وتعتبر من الخطوط الحمراء كالزواج العرفي الذي يسعى إليه وناس، واعتبار تعدد الزوجات أمر شرعي لا غبار عليه. 
+
+</div></div></a><a class="btn" href="http://aliwix-play.tn:8080/script/fck1.php?id=https://vidbam.org/embed-gva153byk5ai.html"><img class="thumb" src="https://r.yaqot.me/upload/articles/5fcea399.png">');
+document.write('  <div class="video-8">');
+document.write('    <i class="bi bi-play-btn-fill"></i>');
+document.write('    <div class="episode">
+  الحلقة 1
+</div>');
+document.write('  </div>');
+document.write('</a>');
+document.write('<a class="btn" href="http://aliwix-play.tn:8080/script/fck1.php?id=https://vidbam.org/embed-obq46wnmbktv.html?i=1">');
+document.write('  <img class="thumb" src="https://r.yaqot.me/upload/articles/5fcea399.png">');
+document.write('  <div class="video-8">');
+document.write('    <i class="bi bi-play-btn-fill"></i>');
+document.write('    <div class="episode">
+  الحلقة 2
+</div>');
+document.write('  </div>');
+document.write('</a>');
+
+
+
+
+
+
+
+
+
+
+
+
+document.write('</div>');
+}
+
