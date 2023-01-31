d4c9136e14f5f1cb553230749dea0c6cfbe62ab4
HeNy Bn-Hamed <59735422+HeNy007@users.noreply.github.com>
2023-01-31 19:19:32 +0100

    Create live.js

---
+++ api/sports/bein/live.js
@@ -0,0 +1,44 @@
+function FUCKUTHIEF(){
+document.write('<ul id="matchKey">');
+
+
+document.write('  <li class="menu2" tabindex="0" onclick="window.location.href=\'https://aliwixplay.tn/player/qaliwix?src=1\'">');
+document.write('    <img title="Aliwix SP" src="https://aliwixplay.tn/api/sports/bein/img/1">');
+document.write('    <span>Live 🔴 1</span>');
+document.write('  </li>');
+document.write('  <li class="menu2" tabindex="0" onclick="window.location.href=\'https://aliwixplay.tn/player/qaliwix?src=2\'">');
+document.write('    <img title="Aliwix SP" src="https://aliwixplay.tn/api/sports/bein/img/2">');
+document.write('    <span>Live 🔴 2</span>');
+document.write('  </li>');
+document.write('  <li class="menu2" tabindex="0" onclick="window.location.href=\'https://aliwixplay.tn/player/qaliwix?src=3\'">');
+document.write('    <img title="Aliwix SP" src="https://aliwixplay.tn/api/sports/bein/img/3">');
+document.write('    <span>Live 🔴 3</span>');
+document.write('  </li>');
+document.write('  <li class="menu2" tabindex="0" onclick="window.location.href=\'https://aliwixplay.tn/player/qaliwix?src=4\'">');
+document.write('    <img title="Aliwix SP" src="https://aliwixplay.tn/api/sports/bein/img/4">');
+document.write('    <span>Live 🔴 4</span>');
+document.write('  </li>');
+document.write('  <li class="menu2" tabindex="0" onclick="window.location.href=\'https://aliwixplay.tn/player/qaliwix?src=5\'">');
+document.write('    <img title="Aliwix SP" src="https://aliwixplay.tn/api/sports/bein/img/5">');
+document.write('    <span>Live 🔴 5</span>');
+document.write('  </li>');
+document.write('  <li class="menu2" tabindex="0" onclick="window.location.href=\'https://aliwixplay.tn/player/qaliwix?src=6\'">');
+document.write('    <img title="Aliwix SP" src="https://aliwixplay.tn/api/sports/bein/img/6">');
+document.write('    <span>Live 🔴 6</span>');
+document.write('  </li>');
+document.write('  <li class="menu2" tabindex="0" onclick="window.location.href=\'https://aliwixplay.tn/player/qaliwix?src=7\'">');
+document.write('    <img title="Aliwix SP" src="https://aliwixplay.tn/api/sports/bein/img/7">');
+document.write('    <span>Live 🔴 7</span>');
+document.write('  </li>');
+
+
+
+
+
+
+
+
+
+
+document.write('</ul>');
+} 
