7d77b9c1bb5b071ff2e7d41492872fd2b4d35494
HeNy Bn-Hamed <59735422+HeNy007@users.noreply.github.com>
2023-02-19 13:20:38 +0100

    Create kids.js

---
+++ api/kids.js
@@ -0,0 +1,48 @@
+function FUCKUTHIEF(){
+document.write('<ul id="matchKey">');
+
+
+document.write('<li class="menu2" onclick=https("shls-spacetoon-prod-dub.shahid.net/out/v1/6240b773a3f34cca95d119f9e76aec02/index.m3u8")>');
+document.write('  <img title="KIDS " src="https://raw.githubusercontent.com/fraudiay79/logos/master/sa/spacetoon.png">');
+document.write('  <span>spacetoon</span>');
+document.write('</li>');
+document.write('<li class="menu2" onclick=https("shls-mbc3-prod-dub.shahid.net/out/v1/d5bbe570e1514d3d9a142657d33d85e6/index.m3u8")>');
+document.write('  <img title="KIDS " src="https://tv.taghtia.com/assets/channels/mbc3.png">');
+document.write('  <span>mbc3</span>');
+document.write('</li>');
+document.write('<li class="menu2" onclick=https("shls-cartoon-net-prod-dub.shahid.net/out/v1/dc4aa87372374325a66be458f29eab0f/index.m3u8")>');
+document.write('  <img title="KIDS " src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/Cartoon_Network_Arabic_logo.png/693px-Cartoon_Network_Arabic_logo.png">');
+document.write('  <span>cn arabia</span>');
+document.write('</li>');
+document.write('<li class="menu2" onclick=https("admdn5.cdn.mangomolo.com/majid/smil:majid.stream.smil/playlist.m3u8")>');
+document.write('  <img title="KIDS " src="https://majid.ae/images/logo.png">');
+document.write('  <span>majid kids</span>');
+document.write('</li>');
+document.write('<li class="menu2" onclick=https("5d658d7e9f562.streamlock.net/atfal1.com/atfal2/playlist.m3u8")>');
+document.write('  <img title="KIDS " src="https://tv.taghtia.com/assets/channels/atfal-wa-mawahib.png">');
+document.write('  <span>atfal w mawheb</span>');
+document.write('</li>');
+document.write('<li class="menu2" onclick=https("shls-rotanakids-prod-dub.shahid.net/out/v1/df6e0eb3cdc4410b98209aafc8677cef/index.m3u8")>');
+document.write('  <img title="KIDS " src="https://shahidstatic3.akamaized.net//mediaObject/2020/taha/t2020/rotana-channels/rotana-kids-thumb/original/rotana-kids-thumb.jpg?height=1920&width=1080&version=1">');
+document.write('  <span>Rotana kids</span>');
+document.write('</li>');
+document.write('<li class="menu2" onclick=https("shls-gulli-bil-arabi-prod-dub.shahid.net/out/v1/5454d215afba410c90b233f400730958/index.m3u8")>');
+document.write('  <img title="KIDS " src="https://raw.githubusercontent.com/fraudiay79/logos/master/fr/gulli.png">');
+document.write('  <span>gulli</span>');
+document.write('</li>');
+document.write('<li class="menu2" onclick=https("shls-tiji-tv-prod-dub.shahid.net/out/v1/98f46736bd8c4404b67e4b7a38cc8976/index.m3u8")>');
+document.write('  <img title="KIDS " src="https://raw.githubusercontent.com/fraudiay79/logos/master/fr/tiji.png">');
+document.write('  <span>TIJI</span>');
+document.write('</li>');
+
+
+
+
+
+
+
+
+
+
+document.write('</ul>');
+} 
