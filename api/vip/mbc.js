de34d02431b9b23fe007bc20118e0c521798ee35
HeNy Bn-Hamed <59735422+HeNy007@users.noreply.github.com>
2023-02-26 12:45:48 +0100

    Create mbc.js

---
+++ api/vip/mbc.js
@@ -0,0 +1,64 @@
+function FUCKUTHIEF(){
+document.write('<ul id="matchKey">');
+
+
+document.write('<li class="menu2" onclick=https("shls-mbc1na-prod-dub.shahid.net/out/v1/84ab37e99d6e4b16b33c6600f94f6ccb/index.m3u8")>');
+document.write('  <img title="mbc" src="https://static.epg.best/ae/MBC1.ae.png">');
+document.write('  <span>mbc</span>');
+document.write('</li>');
+document.write('<li class="menu2" onclick=https("aliwix-spammers.herokuapp.com/live.m3u8?s=238")>');
+document.write('  <img title="mbc" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkHKtt3RstrR-VhL2qEzzjmt0Gc6x1Zcpl6Q&usqp=CAU">');
+document.write('  <span>mbc 2</span>');
+document.write('</li>');
+document.write('<li class="menu2" onclick=https("shls-mbc3-eur-prod-dub.shahid.net/out/v1/fce09dd6a967431a871efb3b8dec9f82/index.m3u8")>');
+document.write('  <img title="mbc" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTL_6pNLgd6Ta9M5FdjTESxdZXD-RB5PmxOTA&usqp=CAU">');
+document.write('  <span>mbc 3</span>');
+document.write('</li>');
+document.write('<li class="menu2" onclick=https("shls-mbc4-prod-dub.shahid.net/out/v1/c08681f81775496ab4afa2bac7ae7638/index.m3u8")>');
+document.write('  <img title="mbc" src="https://upload.wikimedia.org/wikipedia/fr/5/57/MBC_4.jpeg">');
+document.write('  <span>mbc 4</span>');
+document.write('</li>');
+document.write('<li class="menu2" onclick=https("shls-mbc5-prod-dub.shahid.net/out/v1/2720564b6a4641658fdfb6884b160da2/index.m3u8")>');
+document.write('  <img title="mbc" src="https://upload.wikimedia.org/wikipedia/ar/thumb/4/48/Mbc5.webp/168px-Mbc5.webp.png">');
+document.write('  <span>mbc 5</span>');
+document.write('</li>');
+document.write('<li class="menu2" onclick=https("shls-iraq-prod-dub.shahid.net/out/v1/c9bf1e87ea66478bb20bc5c93c9d41ea/index.m3u8")>');
+document.write('  <img title="mbc" src="https://www.almowaten.net/wp-content/uploads/2019/01/MBC-AL-IRAQ-Logo.jpg">');
+document.write('  <span>mbc iraq</span>');
+document.write('</li>');
+document.write('<li class="menu2" onclick=https("shls-live-event1-prod-dub.shahid.net/out/v1/8b953cc72af04ef681cd7ec8d999f68b/index.m3u8")>');
+document.write('  <img title="mbc" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVyhotYaTbsFoyh194hLUV-hgRqRmJXyfEyQ&usqp=CAU">');
+document.write('  <span>mbc action</span>');
+document.write('</li>');
+document.write('<li class="menu2" onclick=https("shls-mbcbollywood-prod-dub.shahid.net/out/v1/a79c9d7ef2a64a54a64d5c4567b3462a/index.m3u8")>');
+document.write('  <img title="mbc" src="https://static.epg.best/sa/MBCBollywoodEn.sa.png">');
+document.write('  <span>mbc bollywood</span>');
+document.write('</li>');
+document.write('<li class="menu2" onclick=https("shls-mbcdramaksa-prod-dub.shahid.net/out/v1/ce0f0762d89e4394a856c5fd13e43645/index.m3u8")>');
+document.write('  <img title="mbc" src="https://alnahdanews.com/uploads/images/2020/06/E85Zu.jpeg">');
+document.write('  <span>mbc drama</span>');
+document.write('</li>');
+document.write('<li class="menu2" onclick=https("shls-mbcplusdrama-prod-dub.shahid.net/out/v1/97ca0ce6fc6142f4b14c0a694af59eab/index.m3u8")>');
+document.write('  <img title="mbc" src="https://tvfcdn.com/big/mbc-plus-drama-hd.jpg">');
+document.write('  <span>mbc drama +</span>');
+document.write('</li>');
+document.write('<li class="menu2" onclick=https("shls-masr-prod-dub.shahid.net/out/v1/b7093401da27496797a8949de23f4578/index.m3u8")>');
+document.write('  <img title="mbc" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRe0n70EJd9hOV9yqHUhwoNXMESH9Mq5zh3TA&usqp=CAU">');
+document.write('  <span>mbc masr</span>');
+document.write('</li>');
+document.write('<li class="menu2" onclick=https("shls-masr2-prod-dub.shahid.net/out/v1/f683685242b549f48ea8a5171e3e993a/index.m3u8")>');
+document.write('  <img title="mbc" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCB2a-YpMCFwKZ67Tz0LSmQIyWUscxtLLvSg&usqp=CAU">');
+document.write('  <span>mbc masr 2</span>');
+document.write('</li>');
+document.write('<li class="menu2" onclick=https("shls-mbcpersia-prod-dub.shahid.net/out/v1/bdc7cd0d990e4c54808632a52c396946/index.m3u8")>');
+document.write('  <img title="mbc" src="https://upload.wikimedia.org/wikipedia/commons/8/8f/MBC_Persia_Logo.png">');
+document.write('  <span>mbc persia</span>');
+document.write('</li>');
+document.write('<li class="menu2" onclick=https("shls-wanasah-prod-dub.shahid.net/out/v1/c84ef3128e564b74a6a796e8b6287de6/index.m3u8")>');
+document.write('  <img title="mbc" src="https://www.arabsat.com/Library/ChannelImages/Wanasah.png">');
+document.write('  <span>mbc wanasah</span>');
+document.write('</li>');
+ 
+
+document.write('</ul>');
+} 
