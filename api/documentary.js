aa0cc03f4b0952b3508b93f580d5f232a5a73727
HeNy Bn-Hamed <59735422+HeNy007@users.noreply.github.com>
2023-02-26 12:42:55 +0100

    Create documentary.js

---
+++ api/documentary.js
@@ -0,0 +1,57 @@
+function FUCKUTHIEF(){
+document.write('<ul id="matchKey">');
+
+
+document.write('<li class="menu2" onclick=http("39.134.115.163:8080/PLTV/88888910/224/3221225646/index.m3u8")>');
+document.write('  <img title="documentary" src="http://p1.img.cctvpic.com/photoAlbum/page/performance/img/2015/10/10/1444464753117_563.jpg">');
+document.write('  <span>cctv 9</span>');
+document.write('</li>');
+document.write('<li class="menu2" onclick=http("39.135.138.59:18890/PLTV/88888910/224/3221225645/index.m3u8")>');
+document.write('  <img title="documentary" src="https://upload.wikimedia.org/wikipedia/commons/d/d6/CGTN_Documentary_logo.png">');
+document.write('  <span>cgtn documentary</span>');
+document.write('</li>');
+document.write('<li class="menu2" onclick=https("admdn2.cdn.mangomolo.com/nagtv/smil:nagtv.stream.smil/playlist.m3u8")>');
+document.write('  <img title="documentary" src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d2/Nat_geo_channel_abu_dhabi.png/512px-Nat_geo_channel_abu_dhabi.png">');
+document.write('  <span>nat geo abudhabi</span>');
+document.write('</li>');
+document.write('<li class="menu2" onclick=https("allanf181.github.io/adaptive-streams/streams/kr/NationalGeographicWildKorea.m3u8")>');
+document.write('  <img title="document" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/National_Geographic_Wild_logo.svg/512px-National_Geographic_Wild_logo.svg.png">');
+document.write('  <span>nat geo wild korea</span>');
+document.write('</li>');
+document.write('<li class="menu2" onclick=http("50.7.161.82:8278/streams/d/Natgeowild/playlist.m3u8")>');
+document.write('  <img title="document" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/National_Geographic_Wild_logo.svg/512px-National_Geographic_Wild_logo.svg.png">');
+document.write('  <span>nat geo wild middle east</span>');
+document.write('</li>');
+document.write('<li class="menu2" onclick=https("okkotv-live.cdnvideo.ru/channel/NGC_Wild_HD.m3u8")>');
+document.write('  <img title="document" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/National_Geographic_Wild_logo.svg/512px-National_Geographic_Wild_logo.svg.png">');
+document.write('  <span>nat geo wild russia</span>');
+document.write('</li>');
+document.write('<li class="menu2" onclick=http("51.210.199.23/hls/stream.m3u8")>');
+document.write('  <img title="document" src="https://i.imgur.com/ZrmiMKy.png">');
+document.write('  <span>persiana documentary</span>');
+document.write('</li>');
+document.write('<li class="menu2" onclick=https("rt-rtd.rttv.com/live/rtdoc/playlist.m3u8")>');
+document.write('  <img title="document " src="https://i.imgur.com/XJGki3v.png">');
+document.write('  <span>rt documentary</span>');
+document.write('</li>');
+document.write('<li class="menu2" onclick=http("185.105.4.106:1935/live/Bahrain%20International/chunklist.m3u8")>');
+document.write('  <img title="documentary " src="https://yt3.ggpht.com/ytc/AMLnZu8_t2dXqMEL5AvO3beyEVfPlMnLDYW7MzlruBC-=s900-c-k-c0x00ffffff-no-rj">');
+document.write('  <span>bahrain documentary</span>');
+document.write('</li>');
+document.write('<li class="menu2" onclick=http("partwo.cdn.mangomolo.com/omcultural/smil:omcultural.stream.smil/playlist.m3u8")>');
+document.write('  <img title="sport" src="https://www.fekera.com/wp-content/uploads/2020/03/IMG_%D9%A2%D9%A0%D9%A2%D9%A0%D9%A0%D9%A3%D9%A1%D9%A5_%D9%A1%D9%A6%D9%A4%D9%A5%D9%A3%D9%A4.jpg">');
+document.write('  <span>oman documentary</span>');
+document.write('</li>');
+document.write('<li class="menu2" onclick=http("82.212.74.2:8000/live/hls/7311.m3u8")>');
+document.write('  <img title="sport" src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/f989bb42489325.5607212e8ec1a.jpg">');
+document.write('  <span>kaifa</span>');
+document.write('</li>');
+document.write('<li class="menu2" onclick=http("185.22.183.147:8085/media/hls_183_147/animal_planet_hq/index.m3u8")>');
+document.write('  <img title="sport" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/2018_Animal_Planet_logo.svg/1200px-2018_Animal_Planet_logo.svg.png">');
+document.write('  <span>animal planet</span>');
+document.write('</li>');
+
+
+
+document.write('</ul>');
+} 
