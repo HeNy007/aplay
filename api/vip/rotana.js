399f05046e630100db9ad62290b09330e48c8b8e
HeNy Bn-Hamed <59735422+HeNy007@users.noreply.github.com>
2023-02-26 12:52:14 +0100

    Create rotana.js

---
+++ api/vip/rotana.js
@@ -0,0 +1,14 @@
+function FUCKUTHIEF(){
+document.write('<ul id="matchKey">');
+
+<li class="menu2" onclick=https("shls-rotanacinema-egy-prod-dub.shahid.net/out/v1/c39c0ecbcbdb46e890e91106776397a8/index.m3u8")><img title="rotana" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-_zFAejuTKITntzWdHbAnAYLquvBhBrkdLw&usqp=CAU"> <span>rotana cinema</span></li> 
+  <li class="menu2" onclick=https("shls-rotanacinema-ksa-prod-dub.shahid.net/out/v1/6cee1c57ea7841e697eb15cefc98e0a6/index.m3u8")><img title="rotana" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-_zFAejuTKITntzWdHbAnAYLquvBhBrkdLw&usqp=CAU"> <span>rotana cinema ksa</span></li> 
+   <li class="menu2" onclick=https("shls-rotanaclassic-prod-dub.shahid.net/out/v1/4eebed211c8441228321b4f67a46c5a5/index.m3u8")><img title="rotana" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-_zFAejuTKITntzWdHbAnAYLquvBhBrkdLw&usqp=CAU"> <span>rotana classic</span></li> 
+    <li class="menu2" onclick=https("shls-rotanadrama-prod-dub.shahid.net/out/v1/20c617b40dc743589ecc9d08d9d3345d/index.m3u8")><img title="rotana" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-_zFAejuTKITntzWdHbAnAYLquvBhBrkdLw&usqp=CAU"> <span>rotana drama</span></li> 
+    <li class="menu2" onclick=https("shls-rotanakhalijia-prod-dub.shahid.net/out/v1/a639fd49db684f1b8c063d398101a888/index.m3u8")><img title="rotana" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-_zFAejuTKITntzWdHbAnAYLquvBhBrkdLw&usqp=CAU"> <span>rotana khalijia</span></li> 
+    <li class="menu2" onclick=https("shls-rotanakids-prod-dub.shahid.net/out/v1/df6e0eb3cdc4410b98209aafc8677cef/index.m3u8")><img title="rotana" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-_zFAejuTKITntzWdHbAnAYLquvBhBrkdLw&usqp=CAU"> <span>rotana kids</span></li> 
+    <li class="menu2" onclick=https("shls-rotanaplus-prod-dub.shahid.net/out/v1/1fc6103458be480b96e6a574b00fe1c0/index.m3u8")><img title="rotana" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-_zFAejuTKITntzWdHbAnAYLquvBhBrkdLw&usqp=CAU"> <span>rotana +</span></li> 
+  <li class="menu2" onclick=https("7jlive.tooli-tv.com/live/701_2.m3u8?session=&location=")><img title="rotana" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-_zFAejuTKITntzWdHbAnAYLquvBhBrkdLw&usqp=CAU"> <span>rotana comedy </span></li> 
+ 
+document.write('</ul>');
+} 
