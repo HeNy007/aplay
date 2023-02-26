3d8bf823f2059f5e87d0d7d469682e0fcee00ba0
HeNy Bn-Hamed <59735422+HeNy007@users.noreply.github.com>
2023-02-26 12:35:54 +0100

    Create music.js

---
+++ api/music.js
@@ -0,0 +1,60 @@
+function FUCKUTHIEF(){
+document.write('<ul id="matchKey">');
+
+
+document.write('<li class="menu2" onclick=https("edge13.vedge.infomaniak.com/livecast/ik:generation-tv/chunklist_w2108992822.m3u8?spark=ff7a88fd-ecd3-4d73-8c6b-b10892751504&token=")>');
+document.write('	<img title=" music " src=" https://cmsphoto.ww-cdn.com/superstatic/36765/art/grande/6553396-9884047.jpg?v=1398160201">');
+document.write('  <span>generation tv</span>');
+document.write('</li>');
+document.write('<li class="menu2" onclick=http("190.2.155.162:8080/mtvfrance/mpegts")>');
+document.write('  <img title="music " src="https://www.tv-direct.fr/assets/logo/chaines/mtv-france.jpg">');
+document.write('  <span>mtv fr</span>');
+document.write('</li>');
+document.write('<li class="menu2" onclick=http("190.2.155.162:8080/mtvhit/mpegts")>');
+document.write('  <img title="music " src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/MTV_Hits_2017_logo.svg/1200px-MTV_Hits_2017_logo.svg.png">');
+document.write('  <span>mtv hits </span>');
+document.write('</li>');
+document.write('<li class="menu2" onclick=http("pluto-live.plutotv.net/egress/chandler/pluto01/live/VIACBS01/master.m3u8")>');
+document.write('  <img title="music " src="https://www.cordcuttersnews.com/wp-content/uploads/2019/07/image001.jpg">');
+document.write('  <span>mtv yo! </span>');
+document.write('</li>');
+document.write('<li class="menu2" onclick=http("pluto-live.plutotv.net/egress/chandler/pluto01/live/VIACBS02/master.m3u8")>');
+document.write('  <img title="music " src="https://www.cordcuttersnews.com/wp-content/uploads/2019/07/image002-1.jpg">');
+document.write('  <span>mtv biggest pop</span>');
+document.write('</li>');
+document.write('<li class="menu2" onclick=http("pluto-live.plutotv.net/egress/chandler/pluto01/live/VIACBS03/master.m3u8")>');
+document.write('  <img title="music " src="https://raw.githubusercontent.com/badboys88888/photo/main/MTVBlockParty.png">');
+document.write('  <span>mtv block party</span>');
+document.write('</li>');
+document.write('<li class="menu2" onclick=http("pluto-live.plutotv.net/egress/chandler/pluto01/live/VIACBS08/master.m3u8")>');
+document.write('  <img title="music " src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/MTV_Video_Music_Awards_logo.svg/1200px-MTV_Video_Music_Awards_logo.svg.png">');
+document.write('  <span>mtv vma</span>');
+document.write('</li>');
+document.write('<li class="menu2" onclick=http("pluto-live.plutotv.net/egress/chandler/pluto01/live/VIACBS07/master.m3u8")>');
+document.write('  <img title="music " src="https://www.cordcuttersnews.com/wp-content/uploads/2019/07/image003.jpg">');
+document.write('  <span>mtv spanking</span>');
+document.write('</li>');
+document.write('<li class="menu2" onclick=https("livelist01.yowi.tv/lista/39596c72840d27b213caf4e58c39599a6f2ed203/master.m3u8")>');
+document.write('  <img title="music " src="https://i.imgur.com/XtUBmwd.png">');
+document.write('  <span>hqm arabic</span>');
+document.write('</li>');
+document.write('<li class="menu2" onclick=https("livelist01.yowi.tv/lista/8327abc87895df4c76db1155435fdca6a3607bbd/master.m3u8")>');
+document.write('  <img title="music " src="https://i.imgur.com/Gbthc2P.png">');
+document.write('  <span>hqm hiphop</span>');
+document.write('</li>');
+document.write('<li class="menu2" onclick=https("livelist01.yowi.tv/lista/5e2db2017a8fd03f73b40ede363d1a586db4e9a6/master.m3u8")>');
+document.write('  <img title="music " src="https://i.imgur.com/rm6TsqZ.png">');
+document.write('  <span>hqm hits</span>');
+document.write('</li>');
+document.write('<li class="menu2" onclick=https("livelist01.yowi.tv/lista/9a4da7871ec57b4b63ed49597a13d09869172be0/master.m3u8")>');
+document.write('  <img title="music " src="https://i.imgur.com/85Km7Jp.png">');
+document.write('  <span>hqm latin</span>');
+document.write('</li>');
+document.write('<li class="menu2" onclick=https("livelist01.yowi.tv/lista/eb2fa68a058a701fa5bd2c80f6c8a6075896f71d/master.m3u8")>');
+document.write('  <img title="music " src="https://i.imgur.com/cfHWiSS.png">');
+document.write('  <span>hqm pop</span>');
+document.write('</li>');
+
+
+document.write('</ul>');
+} 
