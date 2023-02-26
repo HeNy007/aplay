30ed902ca0bc4e5133c85305a593bb4f2c1179df
HeNy Bn-Hamed <59735422+HeNy007@users.noreply.github.com>
2023-02-26 12:48:17 +0100

    Create weyyak.js

---
+++ api/vip/weyyak.js
@@ -0,0 +1,40 @@
+function FUCKUTHIEF(){
+document.write('<ul id="matchKey">');
+
+document.write('<li class="menu2" onclick=https("weyyak-live.akamaized.net/weyyak_action/index.m3u8")>');
+document.write('  <img title="sport" src="https://content.weyyak.com/207c8580-318d-ea11-831d-02d662556796/poster-image">');
+document.write('  <span>weyyek action</span>');
+document.write('</li>');
+document.write('<li class="menu2" onclick=https("weyyak-live.akamaized.net/weyyak_drama/index.m3u8")>');
+document.write('  <img title="sport" src="https://content.weyyak.com/d995517f-bc87-ea11-831d-02d662556796/poster-image">');
+document.write('  <span>weyyek drama</span>');
+document.write('</li>');
+document.write('<li class="menu2" onclick=https("weyyak-live.akamaized.net/weyyak_mix/index.m3u8")>');
+document.write('  <img title="sport" src="https://content.weyyak.com/bc777336-328d-ea11-831d-02d662556796/poster-image">');
+document.write('  <span>weyyek mix</span>');
+document.write('</li>');
+document.write('<li class="menu2" onclick=https("weyyak-live.akamaized.net/weyyak_nawaem/index.m3u8")>');
+document.write('  <img title="sport" src="https://content.weyyak.com/d867c2e9-328d-ea11-831d-02d662556796/poster-image">');
+document.write('  <span>weyyek nawaem</span>');
+document.write('</li>');
+document.write('<li class="menu2" onclick=https("weyyak-live.akamaized.net/weyyak_zee_aflam/index.m3u8")>');
+document.write('  <img title="sport" src="https://pbs.twimg.com/profile_images/958799374569672704/qbV4a4E_.jpg">');
+document.write('  <span>zee aflam</span>');
+document.write('</li>');
+document.write('<li class="menu2" onclick=https("weyyak-live.akamaized.net/weyyak_zee_alwan/index.m3u8")>');
+document.write('  <img title="sport" src="https://pbs.twimg.com/media/DU5XRzKWkAIBAg5.jpg">');
+document.write('  <span>zee alwan</span>');
+document.write('</li>');
+document.write('<li class="menu2" onclick=https("weyyak-live.akamaized.net/weyyak_ten_tv/tracks-v1a1/mono.m3u8")>');
+document.write('  <img title="sport" src="https://content.weyyak.com/8ff99681-4dd8-4e97-aa18-de4a866ef0fd/poster-image?c=23216?d=600%C3%97315">');
+document.write('  <span>ten tv</span>');
+document.write('</li>');
+document.write('<li class="menu2" onclick=https("weyyak-live.akamaized.net/weyyak_smashi_tv/tracks-v1a1/mono.m3u8")>');
+document.write('  <img title="sport" src="https://smashi.tv/assets/img/logo-png-ar.png">');
+document.write('  <span>smashi tv </span>');
+document.write('</li>');
+
+
+
+document.write('</ul>');
+} 
