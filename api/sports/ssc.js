b78eebf49f96ef260b22b98f75d6f324eff63258
HeNy Bn-Hamed <59735422+HeNy007@users.noreply.github.com>
2023-02-27 17:05:44 +0100

    Create ssc.js

---
+++ api/sports/ssc.js
@@ -0,0 +1,38 @@
+function FUCKUTHIEF(){
+document.write('<ul id="matchKey">');
+
+
+
+document.write('<li class="menu2" tabindex="0" onclick="window.location.href=\'http://aliwixplay.rf.gd/script/live3.m3u8?id=746\'">');
+document.write('  <img title="SCCSPORT " src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkw8fktVTQnQiTIsH1dixlWNL69E-NLVpdtg&usqp=CAU">');
+document.write('  <span>SCC Sport 1</span>');
+document.write('</li>');
+document.write('<li class="menu2" tabindex="0" onclick="window.location.href=\'http://aliwixplay.rf.gd/script/live3.m3u8?id=747\'">');
+document.write('  <img title="SCCSPORT " src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkw8fktVTQnQiTIsH1dixlWNL69E-NLVpdtg&usqp=CAU">');
+document.write('  <span>SCC Sport 2</span>');
+document.write('</li>');
+document.write('<li class="menu2" tabindex="0" onclick="window.location.href=\'http://aliwixplay.rf.gd/script/live3.m3u8?id=748\'">');
+document.write('  <img title="SCCSPORT " src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkw8fktVTQnQiTIsH1dixlWNL69E-NLVpdtg&usqp=CAU">');
+document.write('  <span>SCC Sport 3</span>');
+document.write('</li>');
+document.write('<li class="menu2" tabindex="0" onclick="window.location.href=\'http://aliwixplay.rf.gd/script/live3.m3u8?id=749\'">');
+document.write('  <img title="SCCSPORT " src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkw8fktVTQnQiTIsH1dixlWNL69E-NLVpdtg&usqp=CAU">');
+document.write('  <span>SCC Sport 4</span>');
+document.write('</li>');
+document.write('<li class="menu2" tabindex="0" onclick="window.location.href=\'http://aliwixplay.rf.gd/script/live3.m3u8?id=751\'">');
+document.write('  <img title="SCCSPORT " src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkw8fktVTQnQiTIsH1dixlWNL69E-NLVpdtg&usqp=CAU">');
+document.write('  <span>SCC Sport 5</span>');
+document.write('</li>');
+document.write('<li class="menu2" tabindex="0" onclick="window.location.href=\'http://aliwixplay.rf.gd/script/live3.m3u8?id=751\'">');
+document.write('  <img title="SCCSPORT " src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkw8fktVTQnQiTIsH1dixlWNL69E-NLVpdtg&usqp=CAU">');
+document.write('  <span>SCC Sport 6</span>');
+document.write('</li>');
+document.write('<li class="menu2" tabindex="0" onclick="window.location.href=\'http://aliwixplay.rf.gd/script/live3.m3u8?id=752\'">');
+document.write('  <img title="SCCSPORT " src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkw8fktVTQnQiTIsH1dixlWNL69E-NLVpdtg&usqp=CAU">');
+document.write('  <span>SCC Sport 7</span>');
+document.write('</li>');
+
+
+
+document.write('</ul>');
+} 
