6d64d95e2fd8e9b607f370a2011d72778b43fec7
HeNy Bn-Hamed <59735422+HeNy007@users.noreply.github.com>
2023-02-27 16:53:20 +0100

    Create dubaisport.js

---
+++ api/sports/dubaisport.js
@@ -0,0 +1,31 @@
+function FUCKUTHIEF(){
+document.write('<ul id="matchKey">');
+
+
+
+
+document.write('<li class="menu2" onclick=https("dmisvthvll.cdn.mangomolo.com/events/smil:events.smil/chunklist.m3u8")>');
+document.write('  <img title="DUBAISPORT " src="https://i.imgur.com/eytrf0Y.png">');
+document.write('  <span>dubai racing</span>');
+document.write('</li>');
+document.write('<li class="menu2" onclick=https("dmisvthvll.cdn.mangomolo.com/events/smil:events.smil/chunklist.m3u8")>');
+document.write('  <img title="DUBAISPORT " src="https://i.imgur.com/eytrf0Y.png">');
+document.write('  <span>dubai racing 2</span>');
+document.write('</li>');
+document.write('<li class="menu2" onclick=https("dmitnthvll.cdn.mangomolo.com/dubaisports/smil:dubaisports.smil/index.m3u8")>');
+document.write('  <img title="DUBAISPORT " src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsHrCOF1PqaXiKdiezffTYBY3cweM-GoQVgA&usqp=CAU">');
+document.write('  <span>dubai sport 1</span>');
+document.write('</li>');
+document.write('<li class="menu2" onclick=https("dmitnthvll.cdn.mangomolo.com/dubaisportshd5/smil:dubaisportshd5.smil/index.m3u8")>');
+document.write('  <img title="DUBAISPORT " src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsHrCOF1PqaXiKdiezffTYBY3cweM-GoQVgA&usqp=CAU">');
+document.write('  <span>dubai sport 2</span>');
+document.write('</li>');
+document.write('<li class="menu2" onclick=https("dmitnthvll.cdn.mangomolo.com/dubaisportshd5/smil:dubaisportshd5.smil/index.m3u8")>');
+document.write('  <img title="DUBAISPORT " src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsHrCOF1PqaXiKdiezffTYBY3cweM-GoQVgA&usqp=CAU">');
+document.write('  <span>dubai sport 3</span>');
+document.write('</li>');
+
+
+
+document.write('</ul>');
+} 
