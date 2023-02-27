11686d1c23a03ee306a9714aeff34cc1753a3511
HeNy Bn-Hamed <59735422+HeNy007@users.noreply.github.com>
2023-02-27 16:46:17 +0100

    Create adsport.js

---
+++ api/sports/adsport.js
@@ -0,0 +1,24 @@
+function FUCKUTHIEF(){
+document.write('<ul id="matchKey">');
+
+document.write(' <li class="menu2" onclick=https("admdn1.cdn.mangomolo.com/adsports1/smil:adsports1.stream.smil/playlist.m3u8")>');
+document.write('   <img title="ADSPORT " src="https://i.imgur.com/lKl2wZH.png">');
+document.write('   <span>AD sport 1</span>');
+document.write(' </li>');
+document.write(' <li class="menu2" onclick=https("admdn5.cdn.mangomolo.com/adsports1/smil:adsports2.stream.smil/playlist.m3u8")>');
+document.write('   <img title="ADSPORT " src="https://i.imgur.com/9q8gFsq.png">');
+document.write('   <span>AD sport 2</span>');
+document.write(' </li>');
+document.write(' <li class="menu2" onclick=https("admdn2.cdn.mangomolo.com/adsports3/smil:adsports3.stream.smil/playlist.m3u8")>');
+document.write('   <img title="ADSPORT " src="https://i.imgur.com/U6XDpXY.png">');
+document.write('   <span>AD sport 3</span>');
+document.write(' </li>');
+document.write(' <li class="menu2" onclick=https("admdn4.cdn.mangomolo.com/adsports4/smil:adsports4.stream.smil/playlist.m3u8")>');
+document.write('   <img title="ADSPORT " src="https://i.imgur.com/8VqomZ5.png">');
+document.write('   <span>AD sport 4</span>');
+document.write(' </li>');
+
+
+
+document.write('</ul>');
+} 
