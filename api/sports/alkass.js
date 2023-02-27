cd6f103495b8f4dc8097e49b1d99fec9d890f1bb
HeNy Bn-Hamed <59735422+HeNy007@users.noreply.github.com>
2023-02-27 17:22:16 +0100

    Create alkass.js

---
+++ api/sports/alkass.js
@@ -0,0 +1,31 @@
+function FUCKUTHIEF(){
+document.write('<ul id="matchKey">');
+
+
+
+document.write('<li class="menu2" tabindex="0" onclick="window.location.href=\'http://aliwixplay.rf.gd/script/alkass.m3u8?ch=one\'">');
+document.write('  <img title="ALKASSSPORT " src="https://upload.wikimedia.org/wikipedia/commons/7/7f/%D8%B4%D8%B9%D8%A7%D8%B1_%D9%82%D9%86%D9%88%D8%A7%D8%AA_%D8%A7%D9%84%D9%83%D8%AA%D8%B3.jpg">');
+document.write('  <span>ALKASS Sport 1</span>');
+document.write('</li>');
+document.write('<li class="menu2" tabindex="0" onclick="window.location.href=\'http://aliwixplay.rf.gd/script/alkass.m3u8?ch=tow\'">');
+document.write('  <img title="ALKASSSPORT " src="https://upload.wikimedia.org/wikipedia/commons/7/7f/%D8%B4%D8%B9%D8%A7%D8%B1_%D9%82%D9%86%D9%88%D8%A7%D8%AA_%D8%A7%D9%84%D9%83%D8%AA%D8%B3.jpg">');
+document.write('  <span>ALKASS Sport 2</span>');
+document.write('</li>');
+document.write('<li class="menu2" tabindex="0" onclick="window.location.href=\'http://aliwixplay.rf.gd/script/alkass.m3u8?ch=three\'">');
+document.write('  <img title="ALKASSSPORT " src="https://upload.wikimedia.org/wikipedia/commons/7/7f/%D8%B4%D8%B9%D8%A7%D8%B1_%D9%82%D9%86%D9%88%D8%A7%D8%AA_%D8%A7%D9%84%D9%83%D8%AA%D8%B3.jpg">');
+document.write('  <span>ALKASS Sport 3</span>');
+document.write('</li>');
+document.write('<li class="menu2" tabindex="0" onclick="window.location.href=\'http://aliwixplay.rf.gd/script/alkass.m3u8?ch=four\'">');
+document.write('  <img title="ALKASSSPORT " src="https://upload.wikimedia.org/wikipedia/commons/7/7f/%D8%B4%D8%B9%D8%A7%D8%B1_%D9%82%D9%86%D9%88%D8%A7%D8%AA_%D8%A7%D9%84%D9%83%D8%AA%D8%B3.jpg">');
+document.write('  <span>ALKASS Sport 4</span>');
+document.write('</li>');
+document.write('<li class="menu2" tabindex="0" onclick="window.location.href=\'http://aliwixplay.rf.gd/script/alkass.m3u8?ch=five\'">');
+document.write('  <img title="ALKASSSPORT " src="https://upload.wikimedia.org/wikipedia/commons/7/7f/%D8%B4%D8%B9%D8%A7%D8%B1_%D9%82%D9%86%D9%88%D8%A7%D8%AA_%D8%A7%D9%84%D9%83%D8%AA%D8%B3.jpg">');
+document.write('  <span>ALKASS Sport 5</span>');
+document.write('</li>');
+
+
+
+
+document.write('</ul>');
+} 
